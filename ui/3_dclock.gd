b5b7d988ef71693953a644979fa4d2a37e1d51f8
Kay <150719310+KayEntry17@users.noreply.github.com>
2026-07-23 16:24:45 +0100

    set up cameras for transition

--- ui/3_dclock.gd
+++ ui/3_dclock.gd
@@ -4,16 +4,27 @@ extends Node3D
 @export var starttime:Vector3
 @export var timeeff:float
 var startc
+@export var rapid:bool;
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	time=starttime.x*60*60+starttime.y*60+starttime.z
 	startc=time
-
+	timeeff=0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	#time+=delta
-	$hour.rotation_degrees.y=-time/120
-	$min.rotation_degrees.y=-time/10
-	$sec.rotation_degrees.y=-time*6
-	timeeff=time-startc
+	#timeeff=time-startc
+	time=time-int(time/43200)*43200
+	if rapid:
+		time+=delta*1000.0
+		$hour.rotation_degrees.y=(-time/120-int(-time/120/360)*360)
+		$min.rotation_degrees.y=(-time/10-int(-time/10/360)*360)
+		$sec.rotation_degrees.y=(-time*6-int(-time*6/360)*360)
+	else:
+		time=startc+timeeff
+		$hour.rotation_degrees.y=lerp($hour.rotation_degrees.y,(-time/120-int(-time/120/360)*360),4*delta)
+		$min.rotation_degrees.y=lerp($min.rotation_degrees.y,(-time/10-int(-time/10/360)*360),4*delta)
+		$sec.rotation_degrees.y=lerp($sec.rotation_degrees.y,(-time*6-int(-time*6/720)*720),4*delta)
+
+		
---
+++ ui/camtr.gd
@@ -0,0 +1,31 @@
+extends Camera3D
+@export var targetcam:Array[Node3D]
+@export var curt:int
+@export var movespeed:float=4
+@export var rotspeed:float=5
+var dedof:bool=false
+# Called when the node enters the scene tree for the first time.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+
+func _process(delta: float) -> void:
+			if Input.is_action_just_pressed("ui_left"):
+				curt+=1
+				
+	
+			self.global_position.x=lerp(self.global_position.x,targetcam[curt].global_position.x,movespeed*delta)
+			self.global_position.y=lerp(self.global_position.y,targetcam[curt].global_position.y,movespeed*delta)
+			self.global_position.z=lerp(self.global_position.z,targetcam[curt].global_position.z,movespeed*delta)
+			#print("hjoasdjof"+str(self.global_position))
+			#print(targetcam[curt].global_position)
+			#self.global_rotation=targetcam[curt].global_rotation
+			
+			self.global_rotation.x=lerp_angle(self.global_rotation.x,targetcam[curt].global_rotation.x,5*delta)
+			self.global_rotation.y=lerp_angle(self.global_rotation.y,targetcam[curt].global_rotation.y,5*delta)
+			self.global_rotation.z=lerp_angle(self.global_rotation.z,targetcam[curt].global_rotation.z,5*delta)
+			#self.global_rotation=targetcam[curt].global_rotation
+			#self.global_transform=targetcam[curt].global_transform
+	 		
+	
