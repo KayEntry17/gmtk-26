5ad484766f5fe24fdfa4a57f6348b715411276e4
Kay <150719310+KayEntry17@users.noreply.github.com>
2026-07-23 16:51:53 +0100

    transition animation done

--- ui/camtr.gd
+++ ui/camtr.gd
@@ -1,3 +1,4 @@
+@tool
 extends Camera3D
 @export var targetcam:Array[Node3D]
 @export var curt:int
@@ -13,18 +14,25 @@ var dedof:bool=false
 func _process(delta: float) -> void:
 			if Input.is_action_just_pressed("ui_left"):
 				curt+=1
+			if Input.is_action_just_pressed("ui_right"):
+				curt-=1
+			
 				
-	
+	#
 			self.global_position.x=lerp(self.global_position.x,targetcam[curt].global_position.x,movespeed*delta)
 			self.global_position.y=lerp(self.global_position.y,targetcam[curt].global_position.y,movespeed*delta)
 			self.global_position.z=lerp(self.global_position.z,targetcam[curt].global_position.z,movespeed*delta)
-			#print("hjoasdjof"+str(self.global_position))
-			#print(targetcam[curt].global_position)
-			#self.global_rotation=targetcam[curt].global_rotation
-			
-			self.global_rotation.x=lerp_angle(self.global_rotation.x,targetcam[curt].global_rotation.x,5*delta)
-			self.global_rotation.y=lerp_angle(self.global_rotation.y,targetcam[curt].global_rotation.y,5*delta)
-			self.global_rotation.z=lerp_angle(self.global_rotation.z,targetcam[curt].global_rotation.z,5*delta)
+			##print("hjoasdjof"+str(self.global_position))
+			##print(targetcam[curt].global_position)
+			##self.global_rotation=targetcam[curt].global_rotation
+			#self.global_rotation.y=lerp_angle(self.global_rotation.y,targetcam[curt].global_rotation.y,rotspeed*delta)
+			#self.global_rotation.x=lerp_angle(self.global_rotation.x,targetcam[curt].global_rotation.x,rotspeed*delta)
+			#
+			#self.global_rotation.z=lerp_angle(self.global_rotation.z,targetcam[curt].global_rotation.z,rotspeed*delta)
+			global_transform.basis = global_transform.basis.slerp(
+			targetcam[curt].global_transform.basis,
+			rotspeed * delta
+)
 			#self.global_rotation=targetcam[curt].global_rotation
 			#self.global_transform=targetcam[curt].global_transform
 	 		
